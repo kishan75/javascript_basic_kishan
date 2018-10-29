f22d9787d6b338c95ed01d726a6705fd6ab1e19a
kishan75 <kishan.chaurasiya.75@gmail.com>
2018-10-29 21:09:02 +0530

    adding 7th question double all elements handle edge case

---
+++ double_elements_edge_case.js
@@ -0,0 +1,10 @@
+var arr1 = [1,1,6,7,"type",60,5,'t',5,5,7,8];
+
+arr1= arr1.map( function (num) 
+	{
+		if(typeof num==Number)
+		  return num*2
+	    else
+	       return num;
+	});
+console.log(arr1);
