2a8dfbcc05c3efb131e33835c1aafce1130683b6
kishan75 <kishan.chaurasiya.75@gmail.com>
2018-10-30 12:52:04 +0530

    correcting 2 space indentation error

--- double_elements_edge_case.js
+++ double_elements_edge_case.js
@@ -1,10 +1,9 @@
 var arr1 = [1,1,6,7,"type",60,5,'t',5,5,7,8];
-
-arr1= arr1.map( function (num) 
-	{
-		if(typeof num==Number)
-		  return num*2
-	    else
-	       return num;
-	});
+arr1  =  arr1.map( function (num) 
+  {
+    if(typeof num==Number)
+      return num*2
+    else
+      return num;
+  });
 console.log(arr1);
--- map_function_double_elements.js
+++ map_function_double_elements.js
@@ -1,7 +1,7 @@
 var arr1 = [1,1,6,7,10,60,5,4,5,5,7,8];
 
 arr1= arr1.map( function (num) 
-	{
-		return num*2
-	});
+  {
+    return num*2
+  });
 console.log(arr1);
